2359630780925dcd54e5448fae50542c7e325456
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-07-09 09:41:02 +0100

    chore: add support for github organisation catalog  auto discovery

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -26,6 +26,7 @@ backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
